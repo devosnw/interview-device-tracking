7cb21a89029a2c01d20b83e2defd583a6acb7ee0
Nic DeVos <nic@devos.me>
2022-09-28 12:50:11 -0600

    Initial commit of create for device repo

--- idt/repositories.py
+++ idt/repositories.py
@@ -1,6 +1,20 @@
+from typing import Mapping, Type
+import uuid
+
+from idt.domains.devices import Device
+
+
+_DEVICES: Mapping[str, Type[Device]] = {}
+
+
 class DeviceRepository:
-    def create(self):
-        raise NotImplementedError()
+    def create(self, device: Type[Device]):
+        if device.id is not None:
+            raise ValueError(f"Device exists id={device.id}")
+
+        device.id = uuid.uuid4()
+        _DEVICES[device.id] = device
+        return device
 
     def delete(self):
         raise NotImplementedError()
--- tests/idt/test_repositories.py
+++ tests/idt/test_repositories.py
@@ -1,6 +1,43 @@
+import pytest
+from pytest_mock import MockerFixture
+
+from idt.domains.devices import Device
 from idt.repositories import DeviceRepository
+
+
+@pytest.fixture
+def device_repository() -> DeviceRepository:
+    return DeviceRepository()
+
+
+@pytest.fixture
+def device() -> Device:
+    return Device()
 
 
 class TestDeviceRepository:
     def test_init(self):
         assert DeviceRepository() is not None
+
+    class TestCreate:
+        def test_existing(self, device_repository: DeviceRepository, device: Device):
+            device.id = "exists"
+
+            with pytest.raises(ValueError) as e:
+                device_repository.create(device)
+
+            assert str(e.value) == "Device exists id=exists"
+
+        def test_success(
+            self,
+            mocker: MockerFixture,
+            device_repository: DeviceRepository,
+            device: Device,
+        ):
+            mocker.patch("idt.repositories.uuid.uuid4", return_value="def-a-uuid")
+            devices = mocker.patch.dict("idt.repositories._DEVICES")
+
+            device = device_repository.create(device)
+
+            assert device.id == "def-a-uuid"
+            assert devices == {"def-a-uuid": device}
